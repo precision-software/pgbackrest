7ab491820c900155b168035d3be0dc954564f92a
John Morris <john@precision-gps.com>
2022-07-25 06:51:57 -0700

    Moved group and password stubs to individual filesl  Started a "path" file which may not get used.

---
+++ src/common/type/path.c
@@ -0,0 +1,3 @@
+//
+// Created by micro on 7/24/2022.
+//
---
+++ src/common/type/path.h
@@ -0,0 +1,8 @@
+//
+// Created by micro on 7/24/2022.
+//
+
+#ifndef PGBACKREST_PATH_H
+#define PGBACKREST_PATH_H
+
+#endif //PGBACKREST_PATH_H
