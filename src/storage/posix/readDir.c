32e012af2e3e80a867ed0424674c07e38089923d
John Morris <john@precision-gps.com>
2022-09-05 19:18:19 -0700

    WIP.  Creating iterator using posix readdir()

--- src/storage/posix/readDir.c
+++ src/storage/posix/readDir.c
@@ -1,6 +1,99 @@
 /***********************************************************************************************************************************
 
 ***********************************************************************************************************************************/
-//
 
 #include "readDir.h"
+
+
+/***********************************************************************************************************************************
+A collection representing all the files in a Posix directory.
+This is a "lazy" iterable, meaning the file info is read from the operating system "on the fly".
+ReadDir does not provide reliable iteration for remote file systems, so it should be copied to a StorageInfoList
+before being passed to upper layers.
+***********************************************************************************************************************************/
+struct ReadDir
+{
+    String *path;                                                   // path to the directory to be read.
+    StorageLevel level;                                             // level of detail to return for each file entry.
+    bool followLinks;                                               // status of link, or status of file the link points to.
+};
+
+// Iterator for scanning through a directory using readdir().
+struct ReadDirItr
+{
+    ReadDir *readDir;                                               // the directory we are scanning.
+    DIR dir;                                                        // File handle to the open directory.
+};
+
+/***********************************************************************************************************************************
+Define a collection of files contained within a directory.
+* @param path the directory we wish to scan
+* @param level what level of detail do we want for each file in directory.
+* @param followLinks Do we want info about the link or about the file the link point to.
+* @return an initialized ReadDir structure. (not a pointer, but the structure itself)
+***********************************************************************************************************************************/
+ReadDir
+newReadDir(String *path, StorageLevel level, bool followLinks)
+{
+    return (ReadDir) {.path = path, .level = level, .followLinks = followLinks};
+}
+
+
+/***********************************************************************************************************************************
+Construct an iterator for scanning a directory.
+* @param this - allocated structure for us to initialize.
+* @param readDir - the ReadDir collection representing the directory to scan.
+*/
+void
+newReadDirItr(ReadDirItr *this, ReadDir *readDir)
+{
+    this->readDir = readDir;
+    this->dir = opendir(readDir->path);
+    if (this->dir == NULL && errno != XXXX)
+        THROW()
+}
+
+/***********************************************************************************************************************************
+Point to the next item in the collection, returning NULL if there are no more.
+***********************************************************************************************************************************/
+StorageInfo *
+nextReadDirItr(ReadDirItr *this)
+{
+    if (this->dir == NULL)
+        return NULL;
+
+    // Get the next entry, checking for errors and EOF.
+    errno = 0;                                                      // As recommended by Posix standard for readdir()
+    dirent *entry = readdir(this->dir);
+    if (entry == NULL && errno != 0)
+        THROW();
+    if (entry == NULL)
+        return NULL;
+
+    // Fetch extended info about the file, throwing exception if there are problems.
+    this->info = direntToInfo(this, entry);
+
+    // Return a pointer to information about the file.
+    return *this->info;
+}
+
+/***********************************************************************************************************************************
+destroy the iterator, closing the directory if it was successfully opened.
+***********************************************************************************************************************************/
+void
+destructReadDirItr(ReadDirItr *this)
+{
+    if (this->dir != NULL)
+        closedir(this->dir);  // Could generate error.
+}
+
+/***********************************************************************************************************************************
+Given the limited info about a file we got from readdir(), gather additional info and convert to a StorageInfo structure.
+***********************************************************************************************************************************/
+void StorageInfo
+direntToInfo(ReadDirItr *this, dirent *entry)
+{
+
+}
+
+#endif //PGBACKREST_READDIR_H
--- src/storage/posix/readDir.h
+++ src/storage/posix/readDir.h
@@ -6,77 +6,19 @@ Describe file.
 
 // A collection representing all the files in a Posix directory.
 //; This is a "lazy" iterable, meaning the file info is generated "on the fly".
-//  Does not provide reliable iteration for remote file systems,
-//  so it should be buffered before being passed to upper layers.
-typedef struct ReadDirRaw
-{
-    String *path;
-    StorageLevel level;
-    bool followLinks;
-} ReadDirRaw;
+//  Does not provide reliable iteration for remote file systems, so it should be buffered before being passed to upper layers.
+struct ReadDir;
+struct ReadDirItr;
+typedef struct ReadDir ReadDir;
+typedef struct ReadDirItr ReadDirItr;
 
-Inline ReadDirRaw *
-newReadDirRaw(String *path, StorageLevel level, bool followLinks)
-{
-    return &(ReadDirRaw) {.path = path, .level = level, .followLinks = followLinks};
-}
+// Create a collection for scanning a posix directory using readdir();
+ReadDir newReadDir(String *path, StorageLevel level, bool followLinks);
 
-
-typedef struct ReadDirRawItr
-{
-    ReadDirRaw *readDir;
-    Dir *dir;
-    int errno;
-} ReadDirRawItr;
-
-INLINE void
-newItrReadDirRaw(ReadDirRawItr *this, ReadDirRaw *readDir)
-{
-    this->readDir = readDir;
-    this->errno = 0;
-    this->dir = opendir(readDir->path);
-    if (this->dir == NULL && errno != XXXX)
-        this->errno = errno
-}
-
-
-INLINE StorageInfo *
-nextReadDirRawItr(ReadDirRawItr *this)
-{
-    if (this->dir == NULL || this->errno != 0)
-        return NULL;
-
-    // Get the next entry. If error, save the error code and stop iterating.
-    errno = 0;                                                      // As recommended by Posix standard for readdir()
-    dirent *entry = readdir(this->dir);
-    if (entry == NULL) {
-        this->errno = errno;
-        return NULL;
-    }
-
-    // Fetch extended info about the file, setting errno and stopping iteration if problems.
-    StorageInfo info = direntToInfo(this, entry);
-    if (this->errno != 0)
-        return NULL;
-
-}
-
-INLINE void
-destruct(ReadDirRawItr *this)
-{
-    if (this->dir != NULL)
-        closedir(this->dir);  // Could generate error.
-
-    // Check for errors.
-    if (this->errno != 0) {
-
-        // when opening the directory or getting extended file information.
-        if (this->dir == NULL)
-            THROW();
-        else
-            THROW();
-        }
-}
+// Define Iterator methods on the ReadDir collection.
+void newReadDirItr(ReadDirItr *this, ReadDirRaw *readDir);
+StorageInfo *nextReadDirItr(ReadDirItr *this);
+destructReadDirItr(ReadDirItr *this);
 
 
 #endif //PGBACKREST_READDIR_H
