09985a0f498f0e8f99d2689b59fd9a875c9ef4d9
John Morris <john.morris@crunchydata.com>
2022-10-04 15:14:20 -0700

    Restored problems with scan.h header file.

--- src/storage/scan.h
+++ src/storage/scan.h
@@ -1,8 +1,35 @@
-/****
-****/
+//
 #ifndef STORAGE_SCAN_H
 #define STORAGE_SCAN_H
 
+#include "common/regExp.h"
 
+#include "common/type/collection.h"
+#include "common/type/list.h"
+#include "common/type/string.h"
+#include "storage/list.h"
+#include "storage/storage.h"
 
-#endif //STORAGE_S3_NEWITERATOR_H
+/*******************************************************************************************************************************
+Read the contents of a directory (and subdirectories if .recurse is set), returning a lazy collection of Info for each file.
+***********************************************************************************************************************************/
+#define storageScanP(this, path, ...)                                                                                  \
+    storageScan(this, path, (StorageScanParams){VAR_PARAM_INIT, __VA_ARGS__})
+
+typedef struct StorageScanParams
+{
+    VAR_PARAM_HEADER;
+    StorageInfoLevel level;                                         // Level of detail about each file
+    bool followLink;                                                // Should we follow links or report their contents
+    SortOrder sortOrder;                                            // Order sort the files.
+    bool recursive;                                                 // Should we scan subdirectories?
+    // Below are not implemented yet.
+    String *expression;                                             // A regular expression to match files we want to keep.
+    bool errorOnMissing;                                            // Throw an error if a file is missing by the time we scan it.
+    bool nullOnMissing;                                             // Return NULL if a file is missing
+} StorageScanParams;
+
+Collection *
+storageScan(Storage *this, String *path, struct StorageScanParams param);
+
+#endif //STORAGE_SCAN_H
